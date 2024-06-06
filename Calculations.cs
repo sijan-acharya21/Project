885d20e0e20f692fdd61ee795205c211a5353f13
sijan-acharya21 <136575897+sijan-acharya21@users.noreply.github.com>
2024-06-06 22:47:15 +1000

    Update Calculations.cs

--- Calculations.cs
+++ Calculations.cs
@@ -1,1 +1,24 @@
+// Calculate gross pay
+double hourlyRate;
+double hoursWorked;
+double grossPay = hourlyRate * hoursWorked;
 
+// Calculate net pay
+double netPay;
+double tax;
+double grossPay;
+netPay = grossPay - tax;
+
+// Calculate tax
+// Need to select a, b and x from the tax file sheet
+// Need to check if the employee has claimed tax free threshold or not from tax file sheet
+double a;
+double b;
+double x;
+double tax = ax - b;
+
+// Calculate superannuation
+double superAnnuation;
+double superRate = 11;
+double grossPay;
+superAnnuation = superRate * grossPay;
