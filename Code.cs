83a0f76334c6d09db168259c451b5232aac39f99
sijan-acharya21 <136575897+sijan-acharya21@users.noreply.github.com>
2024-06-09 07:37:03 +1000

    Update Code.cs

--- Code.cs
+++ Code.cs
@@ -5,12 +5,16 @@ Declare the variable which stores file path for tax-rate-withthreshold.csv
 Declare the variable which stores file path for tax-rate-nothreshold.csv
 Declare the variable which stores file path for employee.csv
 
+Conditions
+hourlyRate >= 20.3 && hourlyRate <= 36.88;
+hoursWorked <= 40;
+IF
 
 
 
 
 */
 
-const taxRateWithThresholdFile = "\\Mac\Home\Desktop\Project\Cl_OOProgramming_AE_Pro_Appx\Part 3 application files\taxrate-withthreshold.csv"
-const taxRateNoThresholdFile = "\\Mac\Home\Desktop\Project\Cl_OOProgramming_AE_Pro_Appx\Part 3 application files\taxrate-nothreshold.csv"
-const employeeFile = "\\Mac\Home\Desktop\Project\Cl_OOProgramming_AE_Pro_Appx\Part 3 application files\employee.csv"
+const taxRateWithThresholdFile = "\\Mac\Home\Desktop\Project\Cl_OOProgramming_AE_Pro_Appx\Part 3 application files\taxrate-withthreshold.csv";
+const taxRateNoThresholdFile = "\\Mac\Home\Desktop\Project\Cl_OOProgramming_AE_Pro_Appx\Part 3 application files\taxrate-nothreshold.csv";
+const employeeFile = "\\Mac\Home\Desktop\Project\Cl_OOProgramming_AE_Pro_Appx\Part 3 application files\employee.csv";
