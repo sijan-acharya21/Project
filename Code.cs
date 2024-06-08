115572c6f1579d1edee92272e0b5e361790f2abc
sijan-acharya21 <136575897+sijan-acharya21@users.noreply.github.com>
2024-06-09 05:49:44 +1000

    Update Code.cs

--- Code.cs
+++ Code.cs
@@ -1,1 +1,16 @@
+/* 
+Control flow
 
+Declare the variable which stores file path for tax-rate-withthreshold.csv
+Declare the variable which stores file path for tax-rate-nothreshold.csv
+Declare the variable which stores file path for employee.csv
+
+
+
+
+
+*/
+
+const taxRateWithThresholdFile = "\\Mac\Home\Desktop\Project\Cl_OOProgramming_AE_Pro_Appx\Part 3 application files\taxrate-withthreshold.csv"
+const taxRateNoThresholdFile = "\\Mac\Home\Desktop\Project\Cl_OOProgramming_AE_Pro_Appx\Part 3 application files\taxrate-nothreshold.csv"
+const employeeFile = "\\Mac\Home\Desktop\Project\Cl_OOProgramming_AE_Pro_Appx\Part 3 application files\employee.csv"
